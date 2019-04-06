9fa8837344a910087438679815d808aa78ba2fcb
Ronald J Nagata Jr <junzo@Ronalds-MBP.home>
2019-04-06 13:36:24 -1000

    Adjusted to feedback.

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -9,44 +9,40 @@ module.exports = function(digits){
   var factor_0 = 0;
   var factor_1 = 0;
   var palindromeNumber = 0;
-
   let palFactor1;
   let palFactor2;
-  // do your work here
-  if (digits === 2){
-    factor_0 = 99;
-    factor_1 = 99;
+
+  for(let i = 0; i < digits; i++){
+    factor_0 = parseInt(factor_0.toString() + '9');
+    factor_1 = parseInt(factor_0.toString() + '9');
   }
-  if (digits === 3){
-    factor_0 = 999;
-    factor_1 = 999;
-  }
+
   while(factor_0 >= 10){
     while(factor_1 >= 10){
-      let product = (factor_0 * factor_1).toString();
-      let productReverse = product.split('').reverse().join('');
-      if (product === productReverse){
-        if (parseInt(product) > palindromeNumber){
+      let productString = (factor_0 * factor_1).toString();
+      let reverseString = productString.split('').reverse().join('');
+
+      if (productString === reverseString){
+        let productNum = parseInt(productString);
+
+        if (productNum > palindromeNumber){
           palFactor1 = factor_0;
           palFactor2 = factor_1;
-          palindromeNumber = parseInt(product);
+          palindromeNumber = productNum;
         }
       }
       factor_1--;
     }
-    if (digits === 2){
-      factor_1 = 99;
+
+    for (let i = 0; i < digits; i++){
+      factor_1 = parseInt(factor_1.toString() + '9');
     }
-    if (digits === 3){
-      factor_1 = 999;
-    }
+
     factor_0--;
   }
+
   factor_0 = palFactor1;
   factor_1 = palFactor2;
-  
-
-
 
   return {
     factor_0 : factor_0,
