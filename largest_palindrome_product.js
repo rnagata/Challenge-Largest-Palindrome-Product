409e00f460fe678e78a6a6e87c8a7e275ea44cc9
Ronald J Nagata Jr <junzo@Ronalds-MBP.home>
2019-04-05 20:01:02 -1000

    First Solution

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -10,7 +10,41 @@ module.exports = function(digits){
   var factor_1 = 0;
   var palindromeNumber = 0;
 
+  let palFactor1;
+  let palFactor2;
   // do your work here
+  if (digits === 2){
+    factor_0 = 99;
+    factor_1 = 99;
+  }
+  if (digits === 3){
+    factor_0 = 999;
+    factor_1 = 999;
+  }
+  while(factor_0 >= 10){
+    while(factor_1 >= 10){
+      let product = (factor_0 * factor_1).toString();
+      let productReverse = product.split('').reverse().join('');
+      if (product === productReverse){
+        if (parseInt(product) > palindromeNumber){
+          palFactor1 = factor_0;
+          palFactor2 = factor_1;
+          palindromeNumber = parseInt(product);
+        }
+      }
+      factor_1--;
+    }
+    if (digits === 2){
+      factor_1 = 99;
+    }
+    if (digits === 3){
+      factor_1 = 999;
+    }
+    factor_0--;
+  }
+  factor_0 = palFactor1;
+  factor_1 = palFactor2;
+  
 
 
 
